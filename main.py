2682c9ee61c063c107fc774b9ead171b15a01db3
Ching C Lee <machingclee@gmail.com>
2022-07-04 08:35:31 +0800

    update file structure

---
+++ main.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.nn.functional as F
+from torchsummary import summary
+from torch.utils.tensorboard import SummaryWriter
+from torchtext.datasets
