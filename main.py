aa3928b8a4c0f63348c58fdb78c6a1ea69ac886e
Ching C Lee <machingclee@gmail.com>
2022-07-29 03:16:24 +0800

    structure commit

--- main.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torch.nn as nn
-import torchvision.models as models
-import torch.nn.functional as F
-from torchsummary import summary
-from torch.utils.tensorboard import SummaryWriter
-from torchtext.datasets
